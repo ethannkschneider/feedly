18afb44019068fd80e3c6dc9cd2924b60c168a80
Ethan Schneider <ethannkschneider@gmail.com>
2017-10-24 12:21:06 -0400

    Finish user model auth pattern

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,39 @@ class Users < ApplicationRecord
 
   validates :password, length: { minimum: 6, allow_nil: true }
 
+  after_initialize :ensure_session_token
+
   attr_reader :password
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+
 end
