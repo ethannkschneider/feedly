2c6edfde3888ea47d9fc2a6c714e35eb1a81d1c6
Ethan Schneider <ethannkschneider@gmail.com>
2017-10-27 10:30:26 -0400

    Add appropriate seeds for users, collections, feeds, and subscriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,33 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-CATEGORIES = ['News', 'Tech', 'Politics', 'Sports', 'Philosophy']
+CATEGORIES = [
+  'News', 'Tech', 'Politics', 'Science', 'Sports',
+  'Arts', 'Philosophy', 'Business', 'Entertainment',
+  'Design', 'Marketing', 'Literature', 'Culture' ]
+
+FEEDS = {
+  'NYTimes News' => 'http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml',
+  'NYTimes Politics' => 'http://rss.nytimes.com/services/xml/rss/nyt/Politics.xml',
+  'NYTimes Local' => 'http://rss.nytimes.com/services/xml/rss/nyt/NYRegion.xml',
+  'NYTimes Business' => 'http://rss.nytimes.com/services/xml/rss/nyt/Business.xml',
+  'NYTimes Technology' => 'http://rss.nytimes.com/services/xml/rss/nyt/Technology.xml',
+  'NYTimes Sports' => 'http://rss.nytimes.com/services/xml/rss/nyt/Sports.xml',
+  'NYTimes Science' => 'http://rss.nytimes.com/services/xml/rss/nyt/Science.xml',
+  'NYTimes Arts' => 'http://rss.nytimes.com/services/xml/rss/nyt/Arts.xml',
+  'NYTimes The Stone' => 'http://www.nytimes.com/svc/collections/v1/publish/www.nytimes.com/column/the-stone/rss.xml',
+  'ESPN News' => 'http://www.espn.com/espn/rss/news',
+  'The Ringer' => 'https://www.theringer.com/rss/index.xml',
+  'Boston Globe Local' => 'https://www.boston.com/tag/local-news/feed',
+  'Boston Globe Politics' => 'https://www.boston.com/tag/politics/feed',
+  'Politico Politics' => 'https://www.politico.com/rss/politics08.xml'
+}
 
 # User Seeds (Demo user remains the same)
 User.destroy_all
-demo_user = User.create(email: "readly-demo@demo.com", password: "password", first_name: "Demo", last_name: "last_name")
+demo_user = User.create!(email: "readly-demo@demo.com", password: "password", first_name: "Demo", last_name: "last_name")
 20.times do
-  User.create(
+  User.create!(
     email: Faker::Internet.unique.email,
     password: "password",
     first_name: Faker::Name.first_name,
@@ -23,14 +43,37 @@ end
 
 # Collection Seeds (Demo user's collections remain the same)
 Collection.destroy_all
-
-CATEGORIES.each do |category|
-  Collection.create(name: category, user_id: demo_user.id)
+User.all.each do |user|
+  num_collections = rand(1...CATEGORIES.length)
+  user_categories = CATEGORIES.sample(num_collections)
+  user_categories.each do |category|
+    Collection.create!(name: category, user_id: user.id)
+  end
 end
 
-40.times do
-  Collection.create(
-    name: Faker::Pokemon.unique.move,
-    user_id: User.all.sample.id
-  )
+# Feed Seeds
+Feed.destroy_all
+FEEDS.each do |title, url|
+  Feed.create!(title: title, url: url)
 end
+
+# Feed Subscriptions
+Subscription.destroy_all
+Collection.all.each do |collection|
+  num_feeds = rand(1...FEEDS.length)
+  collection_feeds = Feed.all.sample(num_feeds)
+  collection_feeds.each do |feed|
+    subscription = Subscription.new(feed_id: feed.id, collection_id: collection.id)
+    unless collection.subscriptions.include?(subscription)
+      subscription.save!
+    end
+  end
+end
+
+
+
+
+
+
+
+#
